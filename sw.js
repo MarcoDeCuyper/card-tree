a7728e0b68ddb9b1a8bb743895c04bab0e8e22b1
MarcoDeCuyper <marco@turboprojects.be>
2026-07-15 11:43:43 +0200

    PWA Support - manifest, service worker, and icon

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'card-tree-v1';
+const ASSETS = [
+  '/card-tree/',
+  '/card-tree/index.html',
+  '/card-tree/manifest.json',
+  '/card-tree/icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
